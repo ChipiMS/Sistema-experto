05488d89bb4301405c310eb8974ad50ff4d5854d
angelbalderas <34214510+angelbalderas@users.noreply.github.com>
2018-11-06 19:55:52 -0600

    Falta poco

--- src/sistemaexperto/MotorDeInferencia.java
+++ src/sistemaexperto/MotorDeInferencia.java
@@ -6,7 +6,9 @@ public class MotorDeInferencia {
     BaseDeConocimientos BC;
     BaseDeHechos BH;
     Clausula[] clausulas;
+    String[] variables,hechos;
     Clausula R;
+    int[] conjuntoConflicto;
 
     public MotorDeInferencia(BaseDeConocimientos BC, BaseDeHechos BH) {
         this.BC = BC;
@@ -14,27 +16,39 @@ public class MotorDeInferencia {
     }
     
     public void inferir() throws IOException{
-        String[] hechos=BH.regresaHechos();
+        hechos=BH.regresaHechos();
         clausulas=BC.recuperarSecuencial();
-        int[] conjuntoConflicto={1};
+        conjuntoConflicto[0]=1;
         String meta="";
         while( !contenida(meta,hechos) && conjuntoConflicto.length>0){
             conjuntoConflicto=equiparar();
             if(conjuntoConflicto.length>0){
-                String nuevosHechos;
+                String[] nuevosHechos;
                 R=resolver(conjuntoConflicto);
-                nuevosHechos=aplicar();
+                nuevosHechos=aplicar(R,hechos);
                 actualizar(hechos,nuevosHechos);
             }
         }
     }
     
     private int[] equiparar(){
-        
+        int[] x={0};
+        /*Regla por regla hacer*/
+        /*si regla no esta marcada o no esta en el conjunto conflicto*/
+            analisar();
+            /*si hay que agregar se agrega*/
+        return x;
     }
     
-    private void extraeRegla(){
-        
+    private String[] analisar(){
+        for (int i = 0; i < hechos.length; i++) {
+            /*Buscar en la base de hechos el primer predicado negado
+            y agregar los que encontraste a variables*/
+            /*mientras existan predicados negados y variables.leng>0*/
+            /*Buscar en la base de hechos
+              si no estan quitalos de variables*/
+        }
+        return variables;
     }
     
     private boolean contenida(String meta,String[] hechos){
@@ -54,5 +68,18 @@ public class MotorDeInferencia {
         }
         R=clausulas[menor];
         return R;
+    }
+    
+    private String[] aplicar(Clausula R,String[] hechos){
+        String[] nHechos;
+        /*sustituir variable del predicado no negado
+          con las variables[] eliminar regla usada del conjunto conflicto*/
+        return nHechos;
+    }
+    
+    private void actualizar(String[] hechos,String[] nuevosHechos){
+        for (int i = 0; i < nuevosHechos.length; i++) {
+            hechos[hechos.length]=nuevosHechos[i];
+        }
     }
 }
