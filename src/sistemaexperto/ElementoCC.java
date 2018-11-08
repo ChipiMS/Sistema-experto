61b4b4d7455feefa0f4542b044d09d5314c353de
Edgar Emmanuel Martínez Santibáñez <eemsh2@hotmail.com>
2018-11-07 20:41:04 -0600

    Conjunto conflicto

---
+++ src/sistemaexperto/ElementoCC.java
@@ -0,0 +1,10 @@
+package sistemaexperto;
+import java.util.ArrayList;
+public class ElementoCC{
+    Clausula clausula;
+    ArrayList<ArrayList<String>> variables;
+
+    public ElementoCC() {
+        variables = new ArrayList<ArrayList<String>>();
+    }
+}
