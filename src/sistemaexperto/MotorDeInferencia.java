12951351d18d8be24c334c07435fee61abf84b03
Edgar Emmanuel Martínez Santibáñez <eemsh2@hotmail.com>
2018-11-08 22:39:18 -0600

    Otro cambiecillo

--- src/sistemaexperto/MotorDeInferencia.java
+++ src/sistemaexperto/MotorDeInferencia.java
@@ -1,14 +1,18 @@
 package sistemaexperto;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MotorDeInferencia {
     BaseDeConocimientos BC;
     BaseDeHechos BH;
     Clausula[] clausulas;
     String[] variables,hechos;
-    Clausula R;
-    int[] conjuntoConflicto;
+    ElementoCC R;
+    ConjuntoConflicto conjuntoConflicto;
+    ElementoCC elementoCC;
+    ArrayList<String> nombreVariablesClausula;
+    ArrayList<String> valoresVariablesClausula;
 
     public MotorDeInferencia(BaseDeConocimientos BC, BaseDeHechos BH) {
         this.BC = BC;
@@ -18,45 +22,63 @@ public class MotorDeInferencia {
     public void inferir() throws IOException{
         hechos=BH.regresaHechos();
         clausulas=BC.recuperarSecuencial();
-        conjuntoConflicto[0]=1;
+        conjuntoConflicto = new ConjuntoConflicto();
         String meta="";
-        while( !contenida(meta,hechos) && conjuntoConflicto.length>0){
-            conjuntoConflicto=equiparar();
-            if(conjuntoConflicto.length>0){
+        while(!contenida(meta,hechos) && !conjuntoConflicto.estaVacio()){
+            equiparar();
+            if(!conjuntoConflicto.estaVacio()){
                 String[] nuevosHechos;
-                R=resolver(conjuntoConflicto);
-                nuevosHechos=aplicar(R,hechos);
+                R=resolver();
+                nuevosHechos=aplicar(R);
                 actualizar(hechos,nuevosHechos);
             }
         }
     }
     
-    private int[] equiparar(){
+    private void equiparar(){
         int[] x={0};
         /*Regla por regla hacer*/
         /*si regla no esta marcada o no esta en el conjunto conflicto analisar*/
-        analisar();
-        /*si hay que agregar se agrega*/
+        analisar(clausula);
+        if(elementoCC.nombreVariables != null){ //si hay que agregar se agrega
+            //agregar elemento cc al conjunto conflicto usando la clase de conjunto conflicto
+        }
         return x;
     }
     
-    private String[] analisar(){
+    private String[] analisar(Clausula clausula){
         /*Por cada predicado negado de una regla traer los hechos*/
         /*Todos los hechos del primer predicado ejecutar comparar*/
-        for (int i = 0; i < clausulas.length; i++) {
-            String predicadoNegado=clausulas[i].predicadosNegados[i];
-            String nombrepredicado = predicadoNegado.split("\\(")[0];
-            for (int j = 0; j < hechos.length; j++) {
-                String nombreHecho=hechos[j].split("\\(")[0];
-                if(nombreHecho.equals(nombrepredicado)){
-                    comparar(hechos[j],variables);
+        int i, j;
+        String predicadoNegado, nombrePredicadoNegado, nombreHecho;
+        ArrayList<ArrayList<String>> hechosInvolucrados = new ArrayList<ArrayList<String>>();
+        ArrayList<String> hechosDelPredicado, hechosDelPrimerPredicado;
+        elementoCC = new ElementoCC();
+        elementoCC.clausula = clausula;
+        for(i = 0; i < clausula.predicadosNegados.length; i++){
+            hechosDelPredicado = new ArrayList<String>();
+            hechosInvolucrados.add(hechosDelPredicado);
+            predicadoNegado = clausula.predicadosNegados[i];
+            nombrePredicadoNegado = predicadoNegado.split("\\(")[0];
+            if(predicadoNegado.charAt(0) != '*' && predicadoNegado.charAt(0) != ' '){
+                for(j = 0; j < hechos.length; j++){
+                    nombreHecho = hechos[j].split("\\(")[0];
+                    if(nombreHecho.equals(nombrePredicadoNegado)){
+                        hechosDelPredicado.add(hechos[j]);
+                    }
                 }
             }
+        }
+        hechosDelPrimerPredicado = hechosInvolucrados.get(0);
+        for(i = 0; i < hechosDelPrimerPredicado.size(); i++){
+            nombreVariablesClausula = new ArrayList<String>();
+            valoresVariablesClausula = new ArrayList<String>();
+            comparar(hechosDelPrimerPredicado.get(i), 0, hechosInvolucrados);
         }
         return variables;
     }
     
-    private void comparar(String hecho,String[] variables){
+    private void comparar(String hecho, int predicado, ArrayList<ArrayList<String>> hechosInvolucrados){
         /*variable por variable del hecho*/
         /*si la variable no esta agregada*/
             /*se agrega*/
@@ -69,14 +91,39 @@ public class MotorDeInferencia {
             /*se aplica*/
         /*si no*/
             /*vuelve a comparar*/
-        String[] variableshecho = hecho.split("\\(")[1].split("\\)")[0].split(",");
-        for (int i = 0; i < variables.length; i++) {
-            for (int j = 0; j < variableshecho.length; j++) {
-                if(!variables[i].equals(variableshecho[j])){
-                    variables[variables.length]=variableshecho[j];
-                }else{
-                    
+        int i, j, k, agregados = 0;
+        String[] variablesHecho = hecho.split("\\(")[1].split("\\)")[0].split(",");
+        String[] nombresVariables = elementoCC.clausula.predicadosNegados[predicado].split("\\(")[1].split("\\)")[0].split(",");
+                ArrayList<String> hechosDelSiguientePredicado;
+        boolean variableEncontrada = false;
+        for(i = 0; i < nombresVariables.length; i++) {
+            for(j = 0; j < nombreVariablesClausula.size(); j++){
+                if(nombresVariables[i].equals(nombreVariablesClausula.get(j))){
+                    if(!variablesHecho[i].equals(valoresVariablesClausula.get(j))){ //no coinciden
+                        for(k = 0; k < agregados; k++){
+                            nombreVariablesClausula.remove(nombreVariablesClausula.size()-1);
+                            valoresVariablesClausula.remove(valoresVariablesClausula.size()-1);
+                        }
+                        return;
+                    }
+                    variableEncontrada = true;
                 }
+            }
+            if(!variableEncontrada){ //la variable no está agregada
+                nombreVariablesClausula.add(nombresVariables[i]);
+                valoresVariablesClausula.add(variablesHecho[i]);
+                agregados++;
+            }
+        }
+        if(predicado == hechosInvolucrados.size()-1){ //si es el último predicado negado
+            elementoCC.nombreVariables = (String[])nombreVariablesClausula.toArray();
+            elementoCC.variables.add((String[])valoresVariablesClausula.toArray());
+            conjuntoConflicto.agregarElemento(elementoCC);
+        }
+        else{
+            hechosDelSiguientePredicado = hechosInvolucrados.get(predicado+1);
+            for(i = 0; i < hechosDelSiguientePredicado.size(); i++){
+                comparar(hechosDelSiguientePredicado.get(i), predicado+1, hechosInvolucrados);
             }
         }
     }
@@ -89,21 +136,17 @@ public class MotorDeInferencia {
         return false;
     }
     
-    private Clausula resolver(int[] conjuntoConflicto){
-        int menor=0;
-        for(int i=0; i<conjuntoConflicto.length; i++){
-            if(conjuntoConflicto[i]<menor){
-                menor = conjuntoConflicto[i];
-            }
-        }
-        R=clausulas[menor];
+    private ElementoCC resolver(){
+        ElementoCC R;
+        R = conjuntoConflicto.elementos.get(0);
         return R;
     }
     
-    private String[] aplicar(Clausula R,String[] hechos){
+    private String[] aplicar(ElementoCC R){
         String[] nHechos={};
         /*sustituir variable del predicado no negado
           con las variables[] eliminar regla usada del conjunto conflicto*/
+        conjuntoConflicto.eliminarElemento(R); //eliminar regla usada del conjunto conflicto
         return nHechos;
     }
     
