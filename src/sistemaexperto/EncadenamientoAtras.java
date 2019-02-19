549a405676218e3fa2e0820e21dd0fafc39ef291
Alan <15030093@itcelaya.edu.mx>
2019-02-19 08:21:14 -0600

    metodo inferir aunque falta agregar la llamada a evaluar regla

--- src/sistemaexperto/EncadenamientoAtras.java
+++ src/sistemaexperto/EncadenamientoAtras.java
@@ -1,16 +1,22 @@
 package sistemaexperto;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class EncadenamientoAtras {
     public static boolean inferir(BaseDeConocimientos BC,BaseDeHechos bh,String meta) throws IOException {
         ArrayList<Clausula> reglas=new ArrayList<>();
+        int reply;
         int i;
         reglas=busquedaConsecuente(meta,BC);
         for(i=0;i<reglas.size();i++){
-
+            if("evaluarRegla(meta)")
+                return true;
         }
+        reply=JOptionPane.showConfirmDialog(null,"¿tiene usted"+meta+"?","Pregunta",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+        if(reply== JOptionPane.YES_OPTION)
+            return true;
         return false;
     }
     private static ArrayList<Clausula> busquedaConsecuente(String meta,BaseDeConocimientos BC) throws IOException {
