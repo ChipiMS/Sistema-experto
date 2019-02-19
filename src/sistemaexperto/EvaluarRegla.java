d02e57be7d725077151c73a90ad1e1faf3c2b163
noruego <adrian.ed@hotmail.es>
2019-02-18 22:09:56 -0600

    working on evaluarRegla

--- src/sistemaexperto/EvaluarRegla.java
+++ src/sistemaexperto/EvaluarRegla.java
@@ -13,8 +13,22 @@ public class EvaluarRegla {
     
     BaseDeConocimientos BC;
     BaseDeHechos BH;
-    public static void evaluarRegla(BaseDeConocimientos BC, BaseDeHechos BH)
+    String meta;
+    public static void evaluarRegla(BaseDeConocimientos BC, BaseDeHechos BH, String meta)
     {
        String [] hechos = BH.regresaHechos();
+       sustituir( BC,  BH,  meta);//metodo para sustuir variables de la meta en la regla
+       for(int i=0;i<hechos.length;i++)
+       {
+           evaluarPredicado( BC,  BH,  meta);
+       }
+    }
+    public static void sustituir(BaseDeConocimientos BC, BaseDeHechos BH, String meta)
+    {
+        
+    }
+    public static void evaluarPredicado(BaseDeConocimientos BC, BaseDeHechos BH, String meta)
+    {
+        
     }
 }
