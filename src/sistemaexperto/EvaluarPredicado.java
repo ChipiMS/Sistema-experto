5e95f06ece6029e317d2fa124f0447acf7585373
Armando Barron Garcia <madaraushiha54@gmail.com>
2019-02-20 00:01:59 -0600

    Ya cambie segun lo que me mandaron, pero aun no se si este bien

--- src/sistemaexperto/EvaluarPredicado.java
+++ src/sistemaexperto/EvaluarPredicado.java
@@ -11,41 +11,38 @@ public class EvaluarPredicado {
     public static boolean evaluarPredicado(BaseDeConocimientos BC, BaseDeHechos BH, String meta, String regla) throws IOException {
         boolean es_hecho = false;
         int j, k;
-        String[] metas, hecho, variables;
+        String[]   variables;
         String[] hechos = BH.regresaHechos();
-        String nombre_predicado,nombre_meta;
-
+        String nombre_predicado,nombre_meta,hecho,variables_meta, variable_regla;
+        //Por cada hecho de la base de hechos
         for (j = 0; j < hechos.length; j++) {
-            hecho = hechos[j].split("\\(")[1].split("\\)")[0].split(","); //Extraigo el hecho
-            metas = meta.split("\\(")[1].split("\\)");  //Extraigo la meta suponiendo que asi viene
+            //Extraer el nombre de la meta y del hecho, y si son los mismos
+            hecho = hechos[j].split("\\(")[0]; //nombre hecho                 .split("\\)")[0].split(",")
             nombre_meta = meta.split("\\(")[0];  //Nombre de la meta
-            
-            //Por cada hecho de la base de hechos
-            if (hecho[j].equals(metas)) {
-                nombre_predicado = regla.split("\\(")[0];
-                
-                //Extraer el nombre del predicado y si son los mismos
-                if (nombre_predicado.equals(nombre_meta)) {
-                    variables = regla.split("\\(")[1].split("\\)")[0].split(",");
-                    
-                    //Variable por variable-----Si es igual a la de la meta termina bien
-                    for (int i = 0; i < variables.length; i++) {
-                        if (variables[i].equals(metas)) {
-                            es_hecho = true;
-                        } else {
-                            if (nombre_predicado.equals(metas)) {
-                                  es_hecho = true;
-                            }else
-                            {
-                                es_hecho=false;
-                                EncadenamientoAtras.inferir(BC, BH, meta);
-                            }
-                        }
 
+            //Por cada hecho de la base de hechos
+            if (hecho.equals(nombre_meta)) {
+                //nombre_predicado = regla.split("\\(")[0];
+                for (int v = 0; v < hechos.length; v++) {
+
+                    variables_meta = meta.split("\\(")[0].split("\\)")[0]; // variable de la meta
+                    variables = hechos[v].split("\\(")[1].split("\\)")[0].split(","); //variable del hecho
+                    //Variable por variable de la meta
+                    //Si es igual a la variable del hecho
+                    if (variables[v].equals(variables_meta)) {
+                        es_hecho = true; //Terminar bien
+                    } else {
+                        //Si es igual la variable de la meta y el nombre de la variable en la regla
+                        variable_regla = regla.split("\\(")[0]; //variable de la regla ---AQUI NO ENTENDI MUY BIEN
+                        if (variable_regla.equals(variables_meta)) {
+                            es_hecho = true;//Terminar bien
+                        } else {
+                            es_hecho = false;
+                            EncadenamientoAtras.inferir(BC, BH, meta);
+                        }
                     }
 
                 }
-
             }
 
         }
